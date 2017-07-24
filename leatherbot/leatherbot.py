392ebbb35744e72fa1459252fc54aceb4a91c734
Jason Paris <paris3200@gmail.com>
2017-07-23 22:12:58 -0400

    Added file handler

--- leatherbot/leatherbot.py
+++ leatherbot/leatherbot.py
@@ -23,9 +23,12 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
 formatter = '%(asctime)s:%(levelname)s:%(message)s'
+
 file_handler = logging.FileHandler('moderation.log')
 file_handler.setLevel(logging.INFO)
 file_handler.setFormatter(formatter)
+
+logger.addHandler(file_handler)
 
 subreddit = reddit.subreddit(sub)
 current_time = int(time.time())
